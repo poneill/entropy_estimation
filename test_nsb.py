a2227ec9bc839d495c37059bc99be2ae50d3cfaa
Patrick O'Neill <pon2@umbc.edu>
2014-04-28 19:10:22 -0400

    add h_mle to comparison

--- test_nsb.py
+++ test_nsb.py
@@ -1,8 +1,9 @@
 from nsb import nsb
+from h_mle import h_mle
 from entropy_estimation_utils import *
 
 def test_nsb(alphabet_size=4,sample_size=1000):
     ps = simplex_sample(alphabet_size)
     true_h = h(ps,units='nats')
     xs = sample_ps(ps,sample_size)
-    print true_h,nsb(xs,alphabet_size=alphabet_size)
+    print true_h,h_mle(xs,units='nats'),h_nsb(xs,alphabet_size=alphabet_size)
