75497d14c4a23342fbf51ce863da86feb8cc2a17
Patrick O'Neill <pon2@umbc.edu>
2014-04-28 18:00:29 -0400

    redundant comments

--- test_nsb.py
+++ test_nsb.py
@@ -3,7 +3,7 @@ from entropy_estimation_utils import *
 
 def test_nsb(alphabet_size=4,sample_size=1000):
     ps = simplex_sample(alphabet_size)
-    true_h = h(ps,units='nats') # in nats
+    true_h = h(ps,units='nats')
     xs = sample_ps(ps,sample_size)
     print ps
     print true_h,nsb(xs,alphabet_size=alphabet_size)
