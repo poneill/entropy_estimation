c80602f8e90914a84c020d7db6b9fb0c48b1c58e
Patrick O'Neill <pon2@umbc.edu>
2014-04-28 17:58:28 -0400

    ic test_nsb

---
+++ test_nsb.py
@@ -0,0 +1,9 @@
+from nsb import nsb
+from entropy_estimation_utils import *
+
+def test_nsb(alphabet_size=4,sample_size=1000):
+    ps = simplex_sample(alphabet_size)
+    true_h = h(ps,units='nats') # in nats
+    xs = sample_ps(ps,sample_size)
+    print ps
+    print true_h,nsb(xs,alphabet_size=alphabet_size)
